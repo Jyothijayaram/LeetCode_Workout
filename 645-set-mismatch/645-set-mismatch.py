83d3b258dd6981fe50ef7b221d0b47aefbd2af26
Jyothi Ram <97031682+Jyothijayaram@users.noreply.github.com>
2022-09-01 20:24:05 +0530

    Time: 191 ms (98.65%), Space: 15.7 MB (56.74%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.py
+++ 645-set-mismatch/645-set-mismatch.py
@@ -5,9 +5,11 @@ class Solution:
         missing_num = sum( range( len(nums)+1) ) - sum( set(nums) )
         
         # To find the duplicate number in an array 
-        dup=[k for k,v in Counter(nums).items() if v > 1]
+        dup = sum(nums) - sum(set(nums))
+
+
         
-        return *dup,missing_num
+        return dup,missing_num
         
                 
             
