7fae82e8542c3efcb6a34a020c5fa09ceccc8f9c
Jyothi Ram <97031682+Jyothijayaram@users.noreply.github.com>
2022-09-01 12:15:54 +0530

    Time: 73 ms (18.26%), Space: 13.9 MB (31.93%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,12 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sum=0
-        sum2=0
-        for letter in s:
-            sum=sum+ord(letter)
-        for letter2 in t:
-            sum2=sum2+ord(letter2)
-        return chr(sum2-sum)
+        res=str(s+t)
+        ans=0
+        for letter in res:
+            ans=ans ^ ord(letter)
+            
+        return chr(ans)
         
        
                 
