ee9bef7e89ca9490062beb615725e441bb90f7f3
Jyothi Ram <97031682+Jyothijayaram@users.noreply.github.com>
2022-09-18 19:21:34 +0530

    Time: 343 ms (23.73%), Space: 15.5 MB (85.59%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,17 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        s=set(nums)
-        high=0
-        count=0
-        for ele in s:
-            count=0
-            for j in range(0,len(nums)):
-                if ele==nums[j]:
-                    count+=1
-            if count>high:
-                max_ele=ele
-                high=count
-        return max_ele
+        dic={}
+        for ele in nums:
+            if ele not in dic:
+                dic[ele]=nums.count(ele)
+        return max(dic,key=dic.get)
+        
                     
             
         
