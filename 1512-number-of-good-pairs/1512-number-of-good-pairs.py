3396efc63dfcb96a952edd589f769634f8ac4c63
Jyothi Ram <97031682+Jyothijayaram@users.noreply.github.com>
2022-09-19 15:22:49 +0530

    Time: 35 ms (92.72%), Space: 13.8 MB (56.27%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -4,11 +4,14 @@ class Solution:
         counter=0
         for num in nums:
             if num in dic:
-                counter+=dic[num]
+                # counter+=dic[num]
                 dic[num]+=1
             else:
                 dic[num]=1
+        for val in dic.values():
+            counter=counter + (val * (val-1)) // 2
         return counter
+       
         
         
 
