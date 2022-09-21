25916846d4fa76e7a0d098bc09451f6b7413dcbe
Jyothi Ram <97031682+Jyothijayaram@users.noreply.github.com>
2022-09-21 11:13:59 +0530

    Time: 60 ms (26.26%), Space: 14 MB (11.58%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,12 +1,11 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        h={}
-        
-        for c in sentence:
-            if c not in h: 
-                h[c]=1
-        return sum(h.values())==26
-         
-#         else:
-#             return False
+        character= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for c in character:
+            if c in sentence:
+                continue
+            else:
+                return 0
+        return 1
+                
         
