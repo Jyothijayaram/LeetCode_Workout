0dff0406d8ec1432fb156c7531e21cb023212eda
Jyothi Ram <97031682+Jyothijayaram@users.noreply.github.com>
2022-09-28 18:18:52 +0530

    Time: 112 ms (27.25%), Space: 13.9 MB (32.06%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,3 +1,6 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        return max(map(sum,accounts))
+        large=0
+        for p in accounts:
+            large=max(sum(p), large)
+        return large
