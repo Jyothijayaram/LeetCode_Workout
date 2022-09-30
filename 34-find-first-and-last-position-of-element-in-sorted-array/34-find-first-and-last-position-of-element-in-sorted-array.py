007cce757d1dead568e3f8dd098b24f374a9cf5f
Jyothi Ram <97031682+Jyothijayaram@users.noreply.github.com>
2022-09-30 13:33:22 +0530

    Time: 95 ms (87.68%), Space: 15.4 MB (92.81%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,14 +5,19 @@ class Solution:
         return [left,right]
         
     def bin_search(self,nums,target,leftmost):
+
         i=-1
         start,end=0,len(nums)-1
+
         while(start<=end):
             mid=(start+end)//2
+
             if target > nums[mid]:
                 start=mid+1
+
             elif target < nums[mid]:
                 end=mid-1
+
             else:
                 i=mid
                 if leftmost:
